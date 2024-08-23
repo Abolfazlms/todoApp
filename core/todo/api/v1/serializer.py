9a9f38e523d56416d8224a29305464c3e8568f75
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-08-23 21:14:57 +0330

    api structure are added to proect

---
+++ core/todo/api/v1/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from todo.models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        # fields = ['id','title','is_complete','created_date','updated_date']
+        fields = '__all__' 
