dea411bfaead9e8e1151f9b588c443e5a42ae3f2
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-09-23 09:53:48 +0330

    Refactor : project are ready to test

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
+# from rest_framework.permissions import IsAuthenticated
+# from django.shortcuts import get_object_or_404
 from .serializers import (
     RegisterSerializer,
 )
